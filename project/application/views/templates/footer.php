08cc4f87761e6efd8f14b2fbf1bce74f4a6543b1
Anna Carlson <annacarlson@Annas-MacBook-Pro.local>
2014-02-07 11:34:03 -0800

    adding in a static homepage view for testing

---
+++ project/application/views/templates/footer.php
@@ -0,0 +1,14 @@
+
+<!--ending body tag + container-->
+</div>
+</body>
+
+<!--bootstrap sticky footer-->
+<div id="footer" class="navbar navbar-inner  navbar-fixed-bottom">
+		<div class="container">
+			<p> <center>Anna Carlson&copy;2014</center></p>
+		</div>
+	</div>
+		
+<!--end code-->	
+</html>
---
+++ project/application/views/templates/header.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+	<title>Testing </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+<!--style sheets + js  -->
+    <link href="css/bootstrap.css" rel="stylesheet" media="screen">
+  	<script src="js/angular.min.js">
+  	<script src="http://code.jquery.com/jquery.js"></script>
+  
+</head>
+
+<!--nav bar would go HERE
+
+main body chunk-->
+<div class="container">
+<body>
+	<h1>Testing things - like API</h1>
+
