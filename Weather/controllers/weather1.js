a415a0bd4d6d7af81ee815b70189bce56a2730b3
Anna Carlson <annacarlson@Annas-MacBook-Pro.local>
2014-02-17 18:51:34 -0800

    [delivers #65166456] [delivers #65942464]

--- Weather/controllers/weather1.js
+++ Weather/controllers/weather1.js
@@ -1,10 +1,27 @@
-var weather1 = angular.module('weather1', []);
+var weather1 = angular.module('weather1', ['ngRoute']);
 
-var urlBase = 'http://api.wunderground.com/api/cf6c8139e5b43574/conditions/q/99114.json';
+weather1.config(['$routeProvider', function($routeProvider){
+	$routeProvider
+		.when('/show',{
+			templateUrl:'templates/showWeather.html',
+			controller: 'WeatherController'})
+		.when('/default',{
+			templateUrl:'templates/inputZip.html',
+			})
 
+		.otherwise({
+			redirectTo: '/default'
+		});
+}]);
+  
+  //not working, I broke it. Fix later
 weather1.controller('WeatherController', function($http, $scope){
+	var targetzip = $scope.zipcode;
+	var urlBase = 'http://api.wunderground.com/api/cf6c8139e5b43574/conditions/q/';
+	var call = '.json?callback=JSON_CALLBACK';
+	var request = urlBase.concat(targetzip,call);
 	
-	$http.jsonp('http://api.wunderground.com/api/cf6c8139e5b43574/conditions/q/99114.json?callback=JSON_CALLBACK').success(function(data) {
+	$http.jsonp('request').success(function(data) {
 		$scope.weather = data.current_observation;
 		
 		console.log($scope.weather);
@@ -13,7 +30,14 @@ weather1.controller('WeatherController', function($http, $scope){
   }).error(function(data) {
 	  
  		console.log('fail');
-  });
 });
+});
+
+/*  
+  weather1.controller('inputController', function ($scope, data){
+	  $scope.data = data;
+	  
+	  });
+  */
 
  
--- Weather/controllers/weather2.js
+++ Weather/controllers/weather2.js
@@ -1,4 +1,24 @@
-/*
+/*var weather2 = angular.module('weather2', []);
+
+var urlBase = 'http://api.wunderground.com/api/cf6c8139e5b43574/conditions/q/';
+var zipAdd = zipcode;
+var call = '.json?callback=JSON_CALLBACK';
+  
+  weather2.controller('WeatherController', function($http, $scope){
+	
+	$http.jsonp('http://api.wunderground.com/api/cf6c8139e5b43574/conditions/q/99114.json?callback=JSON_CALLBACK').success(function(data) {
+		$scope.weather = data.current_observation;
+		
+		//console.log($scope.weather);
+		//console.log('success');
+		
+  }).error(function(data) {
+	  
+ 		//console.log('fail');
+  });
+});
+
+
 
 angular.module('weather2', [])
  .factory('weatherFinder', ['$http', function($http) {
