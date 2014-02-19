fb13b6ad654ad6769146800a64b17eae2663f44f
Anna Carlson <annacarlson@Annas-MacBook-Pro.local>
2014-02-18 18:50:51 -0800

    updating things

--- Weather/controllers/weather1.js
+++ Weather/controllers/weather1.js
@@ -1,5 +1,4 @@
 var weather1 = angular.module('weather1', ['ngRoute']);
-
 weather1.config(['$routeProvider', function($routeProvider){
 	$routeProvider
 		.when('/show',{
@@ -14,18 +13,22 @@ weather1.config(['$routeProvider', function($routeProvider){
 		});
 }]);
   
-  //not working, I broke it. Fix later
+  
 weather1.controller('WeatherController', function($http, $scope){
-	var targetzip = $scope.zipcode;
+	var targetzip = $scope.zipcode.values;
 	var urlBase = 'http://api.wunderground.com/api/cf6c8139e5b43574/conditions/q/';
 	var call = '.json?callback=JSON_CALLBACK';
 	var request = urlBase.concat(targetzip,call);
 	
-	$http.jsonp('request').success(function(data) {
+	
+	console.log(targetzip);
+	
+	
+	$http.jsonp('http://api.wunderground.com/api/cf6c8139e5b43574/conditions/q/99114.json?callback=JSON_CALLBACK').success(function(data) {
 		$scope.weather = data.current_observation;
+	
 		
-		console.log($scope.weather);
-		console.log('success');
+		
 		
   }).error(function(data) {
 	  
@@ -33,11 +36,6 @@ weather1.controller('WeatherController', function($http, $scope){
 });
 });
 
-/*  
-  weather1.controller('inputController', function ($scope, data){
-	  $scope.data = data;
-	  
-	  });
-  */
+
 
  
