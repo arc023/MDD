8115247569d8af8fd2758c0f4f7801708dda9a01
Anna Carlson <annacarlson@Annas-MacBook-Pro.local>
2014-02-24 18:36:58 -0800

    [delivers #66389754] [delivers #66389762]

--- Weather/controllers/weather1.js
+++ Weather/controllers/weather1.js
@@ -1,69 +1,54 @@
-var weather1 = angular.module('weather1', ['ngRoute']);
+var weather1 = angular.module('weather1', ['ngRoute', 'ui.bootstrap']);
   
   /* routing configs */
 
 weather1.config(['$routeProvider', function($routeProvider){
 	$routeProvider
-		.when('/show',{
-			templateUrl:'templates/showWeather.html',
-			controller: 'WeatherController'})
 		.when('/default',{
-			templateUrl:'templates/inputZip.html',
-			controller: 'WeatherController'})
+			templateUrl:'templates/welcome.html',
+			})
 		.when('/about',{
 			templateUrl:'templates/about.html',
 			})
-
+			.when('/show',{
+			templateUrl:'templates/showWeather.html',
+			controller: 'WeatherController'})
+	.when('/pictures',{
+			templateUrl:'templates/inputZip.html',
+			controller:'CarouselCtrl'})
 		.otherwise({
 			redirectTo: '/default'
 		});
 }]);
+
   
   /* cntrls */
   
-weather1.controller('WeatherController', function($http, $scope, $location){
-	$scope.list = [];
-	$scope.zipcode = '';
+weather1.controller('WeatherController', function($http, $scope ){
 	
-	$scope.submit = function(){
-		var urlBase = 'http://api.wunderground.com/api/cf6c8139e5b43574/conditions/q/';
-	var call = '.json?callback=JSON_CALLBACK';
-	var request = urlBase.concat($scope.zipcode,call);
-		
-//		console.log($scope.zipcode);
-		if($scope.zipcode){
-			$scope.list.push(this.zipcode);
-			$scope.zipcode='';
-			$location.path('/show');
-			}
-//		console.log(request);
-			$http.jsonp(request).success(function(data) {
+		$http.jsonp('http://api.wunderground.com/api/cf6c8139e5b43574/conditions/q/99114.json?callback=JSON_CALLBACK').success(function(data) {
 		$scope.weather = data.current_observation;
 	console.log($scope.weather);
-		
+	
   }).error(function(data) {
 	  
  		console.log('fail');
 });
-		
-	};
-	
-
-
-	
-	
 });
 
-//Error Checking - works kinda
-/*
-function validateForm()
-{
-var x=inputZip.html["myForm"]["zipcode"].value;
-if (x==null || x=="")
-  {
-  alert("You must enter a zipcode");
-  return false;
-  }
-};
+weather1.controller('CarouselCtrl', function ($scope) {
+	  $scope.myInterval = 5000;
+	$scope.slides = [
+            {image: 'img/welcome.jpg', description: 'Image 00'},
+            {image: 'img/welcome2.jpg', description: 'Image 01'},
+            {image: 'img/main.jpg', description: 'Image 02'},
+            {image: 'img/tower.jpg', description: 'Image 03'},
+            {image: 'img/library.jpg', description: 'Image 04'},
+			{image: 'img/park.jpg', description: 'Image 05'},
+			{image: 'img/post.jpg', description: 'Image 04'}
+        ];
+		 var slides = $scope.slides;
+	
+   
+ });
 
- */
